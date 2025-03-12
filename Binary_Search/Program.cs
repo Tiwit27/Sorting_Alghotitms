dc17b5f0ce3f0974db02b04d7d87b5891216d99e
RL <lojekrafal27@gmail.com>
2025-03-12 12:51:20 +0100

    binary search

---
+++ Binary_Search/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class HelloWorld
+{
+    static int binarySearch(int[] arr, int x)
+    {
+        int low = 0, high = arr.Length - 1;
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == x)
+            {
+                return mid;
+            }
+            if (arr[mid] < x)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] arr = { 2, 3, 4, 10, 40 };
+        int n = arr.Length;
+        int x = 10;
+        int result = binarySearch(arr, x);
+        if (result == -1)
+        {
+            Console.WriteLine("Element is not present in array");
+        }
+        else
+        {
+            Console.WriteLine("Element is present at index: " + result);
+        }
+    }
+}
